46e53c4342078ab52d60e3f423a34ff5d9a5e215
Frederico Takayama <f.takayama2014@gmail.com>
2024-08-17 07:44:48 -0300

    first commit

---
+++ src/test/java/takayama/edu/challenge/deploy_test/DeployTestApplicationTests.java
@@ -0,0 +1,13 @@
+package takayama.edu.challenge.deploy_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
